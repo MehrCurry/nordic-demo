eb5e4bf0c84003ba0b05cd44958d9424f45acda9
Guido Zockoll <gzockoll@gmail.com>
2015-11-04 18:48:51 +0100

    server.port

--- src/test/java/nordic/coding/DeveloperResourceTest.java
+++ src/test/java/nordic/coding/DeveloperResourceTest.java
@@ -3,14 +3,14 @@ package nordic.coding;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.RestAssured.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = NordicApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest("server.port:0")
 public class DeveloperResourceTest {
 
     @Test

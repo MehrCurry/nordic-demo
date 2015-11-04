4f724c55017f03be730924b28b87d87060b8688c
Guido Zockoll <gzockoll@gmail.com>
2015-11-04 08:37:30 +0100

    path added

--- src/main/java/nordic/coding/entity/DeveloperRepository.java
+++ src/main/java/nordic/coding/entity/DeveloperRepository.java
@@ -3,7 +3,7 @@ package nordic.coding.entity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "developer", path = "developer")
 public interface DeveloperRepository extends PagingAndSortingRepository<Developer, Long> {
 
 }

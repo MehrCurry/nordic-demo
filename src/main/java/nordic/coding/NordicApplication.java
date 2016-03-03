cd2bb1661c9214153311398ec6f6c01420cbb51d
Guido Zockoll <gzockoll@gmail.com>
2016-03-03 10:01:26 +0100

    asciidoclet

--- src/main/java/nordic/coding/NordicApplication.java
+++ src/main/java/nordic/coding/NordicApplication.java
@@ -3,6 +3,38 @@ package nordic.coding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * = Asciidoclet
+ *
+ * Sample comments that include `source code`.
+ *
+ * [source,java]
+ * --
+ * public class Asciidoclet extends Doclet {
+ *     private final Asciidoctor asciidoctor = Asciidoctor.Factory.create();
+ *
+ *     @SuppressWarnings("UnusedDeclaration")
+ *     public static boolean start(RootDoc rootDoc) {
+ *         new Asciidoclet().render(rootDoc);
+ *         return Standard.start(rootDoc);
+ *     }
+ * }
+ * --
+ *
+ * [plantuml,EventBus]
+ * ....
+ * EventBus <.up. SnapshotArea
+ * EventBus <.up. S3
+ * AssetCreatedEvent <.down. StorageArea
+ * AssetCreatedEvent <.down. SnapshotArea
+ * AssetCreatedEvent <.down. S3
+ * AssetDeletedEvent <.down. StorageArea
+ * AssetDeletedEvent <.down. SnapshotArea
+ * AssetDeletedEvent <.down. S3
+ * ....
+ *
+ * @author https://github.com/johncarl81[John Ericksen]
+ */
 @SpringBootApplication
 public class NordicApplication {
 

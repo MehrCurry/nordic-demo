328aafe7162cfa9a15f386c36e94e9d0a8e304d7
Guido Zockoll <gzockoll@gmail.com>
2016-03-09 08:15:14 +0100

    languages refactored

--- src/test/java/nordic/coding/entity/DeveloperTest.java
+++ src/test/java/nordic/coding/entity/DeveloperTest.java
@@ -1,6 +1,9 @@
 package nordic.coding.entity;
 
+import com.google.common.collect.ImmutableSet;
 import org.junit.Test;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,13 +14,22 @@ public class DeveloperTest {
 
     @Test
     public void aSimpleTest() {
-        Developer dev = Developer.builder().firstname("JUnit").lastname("Test").programmingLanguage("Java, Ruby").build();
+        Developer dev = Developer.builder()
+                .firstname("JUnit")
+                .lastname("Test")
+                .build();
+        dev.addLanguages("Java, Ruby");
         assertThat(dev.getFirstname()).isEqualTo("JUnit");
         assertThat(dev.getLastname()).isEqualTo("Test");
-        String[] languages = dev.getProgrammingLanguage().split(",");
+        Set<String> languages = dev.getProgrammingLanguages();
         assertThat(languages).contains("Java");
         assertThat(languages).doesNotContain("Scala");
-        assertThat(languages.length).isEqualTo(2);
+        assertThat(languages.size()).isEqualTo(2);
         assertThat(dev.getId()).isNull();
+
+        dev.addLanguages("Groovy, Closure");
+        assertThat(languages).contains("Groovy");
+        assertThat(languages).doesNotContain("Scala");
+        assertThat(languages.size()).isEqualTo(4);
     }
 }

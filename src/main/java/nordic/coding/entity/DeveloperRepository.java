3dbb3b0a4b8a07ca39109672609c0d0973fc6e8e
Guido Zockoll <gzockoll@gmail.com>
2016-03-09 08:31:35 +0100

    embedded mongo (not working)

--- src/main/java/nordic/coding/entity/DeveloperRepository.java
+++ src/main/java/nordic/coding/entity/DeveloperRepository.java
@@ -1,9 +1,10 @@
 package nordic.coding.entity;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "developer", path = "developer")
-public interface DeveloperRepository extends PagingAndSortingRepository<Developer, Long> {
+public interface DeveloperRepository extends Repository<Developer, Long> {
 
 }
